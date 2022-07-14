8a0f8d2fb896a097a9524752021e69c0e30c5a95
unknown <wilhelm.kerle@uni-konstanz.de>
2022-07-14 13:03:25 +0200

    stuff

--- Biorobotics/Code/Server/LineFollower.py
+++ Biorobotics/Code/Server/LineFollower.py
@@ -10,6 +10,7 @@ import RPi.GPIO as GPIO
 
 class LineFollower:
     def __init__(self):
+        self.CSB = False
         self.isSetBack = False
         self.IR01 = 14
         self.IR02 = 15
@@ -31,45 +32,89 @@ class LineFollower:
             if GPIO.input(self.IR03):
                 # Right Sensor On
                 self.LMR = (self.LMR | 1)
+            steer(self.LMR)
+            #handleModeSwitch(self.LMR, self)
 
-            handleModeSwitch(self.LMR)
+def steer(mode):
+  maxV = 2000
+  midV = 1200
+  minV = 0
+  sL = 0
+  sR = 0
+  if mode ==0:
+    sQ = 0
+    #sR = 0
+  elif mode ==1:
+    sL = maxV
+    sR = -midV
+    PWM.setMotorModel(sL,sL,sR,sR)
+  elif mode ==2:
+    sL = maxV
+    sR = maxV
+    PWM.setMotorModel(sL,sL,sL,sL)
+  elif mode ==3:
+    sL = -midV
+    sR = maxV
+    PWM.setMotorModel(sL,sL,sR,sR)
+  elif mode ==4:
+    sR = maxV
+    sL = -midV
+    PWM.setMotorModel(sL,sL,sR,sR)
+  #elif mode ==5:
+  elif mode ==6:
+    sL = maxV
+    sR = -midV
+    PWM.setMotorModel(sL,sL,sR,sR)
+  elif mode ==7:
+    sQ = 0
+    #sR = 0
 
 
-def handleModeSwitch(mode):
+def handleModeSwitch(mode, self):
     if mode == 1:
         # Only right is on -> turn right
         turnRight()
     elif mode == 2:
         # Only middle sensor is on -> full speed!
         PWM.setMotorModel(2000, 2000, 2000, 2000)
-    elif mode == 3:
+#    elif mode == 3:
         # Middle and right sensor on
-        turnRight()
+#        if (self.CSB):
+ #           turnRight()
+  #      self.CSB = not (self.CSB)
+#turnRight()
     elif mode == 4:
         # only left sensor on -> turn left
         turnLeft()
     elif mode == 5:
         # Left and right sensor on -> fun mode?
         PWM.setMotorModel(1000, 1000, 1000, 1000)
-    elif mode == 6:
+#    elif mode == 6:
         # middle and left sensor on -> turn left
-        turnLeft()
-    elif mode == 7:
+#        if (self.CSB):
+ #           turnLeft()
+  #      self.CSB = not (self.CSB)
+#turnLeft()
+    elif mode == 0:
         # pass
-        PWM.setMotorModel(0, 0, 0, 0)
+#        if(self.CSB):
+         setBack(self)
+#        PWM.setMotorModel(0, 0, 0, 0)
+    elif mode == 7:
+        turnLeft()
 
 
 def turnLeft():
-    PWM.setMotorModel(-1500, -1500, -500, -500)
+    PWM.setMotorModel(-2500, -2500, 1000, 1000)
 
 
 def turnRight():
-    PWM.setMotorModel(-500, -500, -1500, -1500)
+    PWM.setMotorModel(1000, 1000, -2500, -2500)
 
 
 def setBack(self):
-    self.isSetBack = True
-    PWM.setMotorModel(-500, -500, -500, -500)
+    self.CSB = False
+    PWM.setMotorModel(-1500, -1500, 1500, 1500)
 
 
 infrared = LineFollower()
